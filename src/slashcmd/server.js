f51f7f75af3f8b81306c7bac413c2ad12703c7b2
Niels Joubert <njoubert@gmail.com>
2018-02-12 23:56:18 -0800

    simple express server that responds to a slash command. use ngrok to forward port, setup slack api correctly too

--- src/slashcmd/server.js
+++ src/slashcmd/server.js
@@ -14,3 +14,41 @@ Supported Commands:
 
 
 */
+
+const Express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = new Express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 8088
+
+app.post('/', (req, res) => {
+  let text = req.body.text;
+  console.log(text);
+  return res.json({
+      "text": "It's 80 degrees right now.",
+      "attachments": [
+        {
+            "text":"Partly cloudy today and tomorrow"
+        }
+    ]
+  });
+});
+
+app.get('/', (req, res) => {
+	console.log(req.body);
+	return res.json({
+    	"text": "It's 80 degrees right now.",
+    	"attachments": [
+        {
+            "text":"Partly cloudy today and tomorrow"
+        }
+    ]
+	});
+});
+
+const server = app.listen(port, () => {  
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
